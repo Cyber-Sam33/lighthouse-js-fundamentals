6f01f4c23fbb18565bbc49ce1cd9f4c23c94082c
Cyber-Sam33 <issasammi@yahoo.com>
2022-12-13 21:55:11 +0000

    sayHello function

---
+++ hello.js
@@ -0,0 +1,15 @@
+// const sayHello = function(){
+//   console.log("Hello, world!");
+// }
+
+// sayHello();
+
+const sayHello = function(name){
+  console.log("Hello, " + name);
+}
+
+sayHello("Sam");
+sayHello("Katie");
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Prospero");
